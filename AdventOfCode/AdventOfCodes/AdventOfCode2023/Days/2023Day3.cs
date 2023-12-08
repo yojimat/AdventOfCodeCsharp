e500c8ecbd08b01ab4237e9f2ac50ed6cd225937
yojimat <31702569+yojimat@users.noreply.github.com>
2023-12-08 20:16:47 -0300

    Day 3 part one

--- AdventOfCode/AdventOfCodes/AdventOfCode2023/Days/2023Day3.cs
+++ AdventOfCode/AdventOfCodes/AdventOfCode2023/Days/2023Day3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -8,6 +9,7 @@ namespace AdventOfCode.AdventOfCodes.AdventOfCode2023.Days;
 public static partial class _2023Day3
 {
     private static int _sumOfPartNumbers;
+    private static int _sumOfGearRatios;
     private static string[] _lines;
 
     public static void ExecuteProgram()
@@ -19,9 +21,193 @@ public static partial class _2023Day3
         {
             var line = _lines[index];
             Solution(line, index);
+            Solution2(line, index);
         }
 
         Console.WriteLine($"The sum of part numbers {_sumOfPartNumbers}");// 76536 91830 332815 591377
+        Console.WriteLine($"The sum of the gear ratios {_sumOfGearRatios}");// 76536 91830 332815 591377
+    }
+
+    private static void Solution2(string line, int lineIndex)
+    {
+        var asteriskIndexes = line.Select((c, asteriskIndex) =>
+        {
+            if (c == '*')
+            {
+                return asteriskIndex;
+            }
+
+            return -1;
+        });
+
+        foreach (var asteriskIndex in asteriskIndexes)
+        {
+            if (asteriskIndex == -1) continue;
+            if (!VerifyIfAsteriskIsValidGear(asteriskIndex, lineIndex, out var gearRatio)) continue;
+            _sumOfGearRatios += gearRatio[0] * gearRatio[1];
+        }
+    }
+
+    private static bool VerifyIfAsteriskIsValidGear(int asteriskIndex, int lineIndex, out List<int> numbersFound)
+    {
+        numbersFound = [];
+
+        FindSidedNumbers(lineIndex, asteriskIndex, numbersFound);    // 4683909 
+        FindUpDownNumbers(lineIndex, asteriskIndex, numbersFound);   // 26619112
+        FindDiagonalNumbers(lineIndex, asteriskIndex, numbersFound); // 54258873
+
+        if (numbersFound.Count == 2) return true;
+
+        numbersFound = null;
+        return false;
+    }
+
+    private static void FindDiagonalNumbers(int lineIndex, int asteriskIndex, ICollection<int> numbersFound)
+    {
+        var leftBound = lineIndex - 1 < 0;
+        var rightBound = lineIndex + 1 >= _lines.Length;
+        var upperLeftChar = leftBound ? ' ' : _lines[lineIndex - 1][asteriskIndex - 1];
+        var bottomLeftChar = rightBound ? ' ' : _lines[lineIndex + 1][asteriskIndex - 1];
+        var upperRightChar = leftBound ? ' ' : _lines[lineIndex - 1][asteriskIndex + 1];
+        var bottomRightChar = rightBound ? ' ' : _lines[lineIndex + 1][asteriskIndex + 1];
+        var offset = 1;
+        var digitsFound = new List<char>();
+
+        if (char.IsDigit(upperLeftChar))
+        {
+            while (asteriskIndex - offset >= 0 && char.IsDigit(_lines[lineIndex - 1][asteriskIndex - offset]))
+            {
+                digitsFound.Add(_lines[lineIndex - 1][asteriskIndex - offset]);
+                offset++;
+            }
+
+            digitsFound.Reverse();
+            numbersFound.Add(int.Parse(string.Concat(digitsFound)));
+        }
+
+        digitsFound.Clear();
+        offset = 1;
+
+        if (char.IsDigit(bottomLeftChar))
+        {
+            while (asteriskIndex - offset >= 0 && char.IsDigit(_lines[lineIndex + 1][asteriskIndex - offset]))
+            {
+                digitsFound.Add(_lines[lineIndex + 1][asteriskIndex - offset]);
+                offset++;
+            }
+
+            digitsFound.Reverse();
+            numbersFound.Add(int.Parse(string.Concat(digitsFound)));
+        }
+
+        digitsFound.Clear();
+        offset = 1;
+
+        if (char.IsDigit(upperRightChar))
+        {
+            while (asteriskIndex + offset < _lines[lineIndex + 1].Length &&
+                   char.IsDigit(_lines[lineIndex - 1][asteriskIndex + offset]))
+            {
+                digitsFound.Add(_lines[lineIndex - 1][asteriskIndex + offset]);
+                offset++;
+            }
+
+            numbersFound.Add(int.Parse(string.Concat(digitsFound)));
+        }
+
+        if (char.IsDigit(bottomRightChar))
+        {
+            while (asteriskIndex + offset < _lines[lineIndex + 1].Length &&
+                   char.IsDigit(_lines[lineIndex + 1][asteriskIndex + offset]))
+            {
+                digitsFound.Add(_lines[lineIndex + 1][asteriskIndex + offset]);
+                offset++;
+            }
+
+            numbersFound.Add(int.Parse(string.Concat(digitsFound)));
+        }
+    }
+
+    private static void FindUpDownNumbers(int lineIndex, int asteriskIndex, ICollection<int> numbersFound)
+    {
+        var leftBound = lineIndex - 1 < 0;
+        var rightBound = lineIndex + 1 >= _lines.Length;
+        var upperChar = leftBound ? ' ' : _lines[lineIndex - 1][asteriskIndex];
+        var bottomChar = rightBound ? ' ' : _lines[lineIndex + 1][asteriskIndex];
+        var leftOffset = 0;
+        var offset = 0;
+        var digitsFound = new List<char>();
+
+        if (char.IsDigit(upperChar))
+        {
+            // -1 is to exit the loop at the first non digit char before incrementing the offset.
+            while (asteriskIndex - leftOffset >= 0 && char.IsDigit(_lines[lineIndex - 1][asteriskIndex - leftOffset - 1]))
+            {
+                leftOffset++;
+            }
+
+            while (asteriskIndex - leftOffset + offset < _lines[lineIndex - 1].Length &&
+                   char.IsDigit(_lines[lineIndex - 1][asteriskIndex - leftOffset + offset]))
+            {
+                digitsFound.Add(_lines[lineIndex - 1][asteriskIndex - leftOffset + offset]);
+                offset++;
+            }
+            numbersFound.Add(int.Parse(string.Concat(digitsFound)));
+        }
+
+        digitsFound.Clear();
+        leftOffset = 0;
+        offset = 0;
+
+        if (!char.IsDigit(bottomChar)) return;
+
+        // -1 is to exit the loop at the first non digit char before incrementing the offset.
+        while (asteriskIndex - leftOffset >= 0 && char.IsDigit(_lines[lineIndex + 1][asteriskIndex - leftOffset - 1]))
+        {
+            leftOffset++;
+        }
+
+        while (asteriskIndex - leftOffset + offset < _lines[lineIndex + 1].Length &&
+               char.IsDigit(_lines[lineIndex + 1][asteriskIndex - leftOffset + offset]))
+        {
+            digitsFound.Add(_lines[lineIndex + 1][asteriskIndex - leftOffset + offset]);
+            offset++;
+        }
+        numbersFound.Add(int.Parse(string.Concat(digitsFound)));
+    }
+
+    private static void FindSidedNumbers(int lineIndex, int asteriskIndex, ICollection<int> numbersFound)
+    {
+        var leftBound = asteriskIndex - 1 < 0;
+        var rightBound = asteriskIndex + 1 >= _lines[lineIndex].Length;
+        var leftChar = leftBound ? ' ' : _lines[lineIndex][asteriskIndex - 1];
+        var rightChar = rightBound ? ' ' : _lines[lineIndex][asteriskIndex + 1];
+        var offset = 1;
+        var digitsFound = new List<char>();
+
+        if (char.IsDigit(leftChar))
+        {
+            while (asteriskIndex - offset >= 0 && char.IsDigit(_lines[lineIndex][asteriskIndex - offset]))
+            {
+                digitsFound.Add(_lines[lineIndex][asteriskIndex - offset]);
+                offset++;
+            }
+            digitsFound.Reverse();
+            numbersFound.Add(int.Parse(string.Concat(digitsFound)));
+        }
+
+        digitsFound.Clear();
+        offset = 1;
+
+        if (!char.IsDigit(rightChar)) return;
+
+        while (asteriskIndex + offset < _lines[lineIndex].Length &&
+               char.IsDigit(_lines[lineIndex][asteriskIndex + offset]))
+        {
+            digitsFound.Add(_lines[lineIndex][asteriskIndex + offset]);
+            offset++;
+        }
+        numbersFound.Add(int.Parse(string.Concat(digitsFound)));
     }
 
     private static void Solution(string line, int lineIndex)
